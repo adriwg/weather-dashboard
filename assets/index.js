64b862c29e8ab9e1f1f3cdf3c7f83868595bf21a
AdrianWong <adrianwg04@gmail.com>
2023-04-07 18:56:55 +0100

    Added logic to store the search history in the local storage, add event listener to the button of the search history, fine tune the layout.

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,40 @@ var baseURL_geo ="http://api.openweathermap.org/geo/1.0/direct?";
 var apiKey = "d06ddbdb382e974460b03c357ca41e3e";
 var baseURL_icn = "http://openweathermap.org/img/w/";
 var city_name = "";
+var searchHistory = [];
+
+$(document).ready(function () {
+
+    init();
+
+    $("#history").on("click", "button", function () {
+        city_name = $(this).attr("data-city");
+        geoInfo(city_name);
+        $("#today").show();
+    });
+    
+});
+
+
+// Check the availability of the local storage at the start up
+function init() {
+    if(localStorage.getItem("history") ==null){
+        searchHistory = [];
+    }else{
+        searchHistory = JSON.parse(localStorage.getItem("history"));
+        displaySearchHistory();
+    }
+}
+
+// Display the search history in HTML
+function displaySearchHistory() {
+    $("#history").empty(); // Clear the current dsiplayed history
+    for (var i = 0; i < searchHistory.length; i++) {
+        var history ="<button class=\"btn btn-secondary\" data-city=\""+searchHistory[i]+"\">"+searchHistory[i]+"</button>";
+        $("#history").append(history);
+    }   
+}
+
 
 // Weather search
 function weatherSearch(event){
@@ -12,6 +46,13 @@ function weatherSearch(event){
         alert("please enter a city name");
     }else{
         geoInfo(city_name);
+        if (!searchHistory.includes(city_name)) {
+            searchHistory.push(city_name);
+            displaySearchHistory();
+            localStorage.setItem("history",JSON.stringify(searchHistory));// update the local storage
+            $("#today").show();
+          }
+        $("#search-input").val("");
     }
 }
 
@@ -78,9 +119,9 @@ function currentWeather(data){
 function displayCurrentWeather(city, date, icn_path, temp, wind, humidity){
     var info = "";
     info += "<h2><span class=\"city\">"+city+"</span> ("+date+") <img src=\""+icn_path+"\"/></h2>";
-    info += "<p>Temp: "+temp+"°C</p>";
+    info += "<p>Temp: "+temp+" °C</p>";
     info += "<p>Wind: "+wind+" KPH</p>";
-    info += "<p>Humidity: "+humidity+"%</p>";
+    info += "<p>Humidity: "+humidity+" %</p>";
     $("#today").html(info);
 }
 
@@ -88,9 +129,10 @@ function displayCurrentWeather(city, date, icn_path, temp, wind, humidity){
 function weatherForecast(data){
     var weatherInfoList = data.list;// list of 5-day weather info with 3-hr steps
     // Create a grid layout for the weather forecast
-    var container = $("<div class=\"container\">");
-    var row = $("<div class=\"row row-cols-5\" id=\"row_cards\">");
-    container.append(row);
+    var container = $("<div class=\"container-fluid\">");
+    var title = $("<h5>5-Day Forecast:</h5>");
+    var row = $("<div class=\"row row-cols-1 row-cols-md-3 row-cols-lg-5 row-cols-xl-5\" id=\"row_cards\">");
+    container.append(title).append(row);
     $("#forecast").html(container);
     
     // To filter daily weather info, as  the data is taking in every 3 hrs, we only pick the weather info at the time 00:00:00
@@ -118,8 +160,8 @@ function displayWeatherForecast(date, icn_path, temp, wind, humidity) {
     var info = "";
     info += "<div class=\"col\">";
     info += "<div class=\"card mb-3\">";
-    info += "<div class=\"card-body\>";
-    info += "<h5 class=\"card-title\">"+dt+"</h5>";
+    info += "<div class=\"card-body\">";
+    info += "<h6 class=\"card-title\">"+dt+"</h6>";
     info += "<p><img src=\""+icn_path+"\"/></p>";
     info += "<p>Temp: "+temp+" °C</p>";
     info += "<p>Wind: "+wind+" KPH</p>";
